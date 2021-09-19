f436ccde09c08566f8ba857d43e83ca44056e496
vin1507 <59875006+vin1507@users.noreply.github.com>
2021-09-19 17:23:41 +0530

    docstring upadated

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -3,40 +3,47 @@ import numpy as np
 import joblib  # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
-
+import logging
 
 plt.style.use("fivethirtyeight")
 
 
 def prepare_data(df):
-    """its is use to seprate the depended and independent feature
-    
+    """it is used to separate the dependent variables and independent features
     Args:
-        df (pd.DataFrame): its the pandas Dataframe
-
+        df (pd.DataFrame): its the pandas DataFrame to
     Returns:
-        type: its return the tuple of depended and independent variable
-            """
+        tuple: it returns the tuples of dependent variables and independent variables
+    """
+    logging.info("Preparing the data by segregating the independent and dependent variables")
     x = df.drop("y", axis=1)
     y = df["y"]
     return x, y
 
 
 def save_model(model, filename):
-    """this saved the trained mode
-    
+    """This saves the trained model to
     Args:
-        model (python object): trained model to 
-        filename (str): trained model to 
+        model (python object): trained model to
+        filename (str): path to save the trained model
     """
+    logging.info("saving the trained model")
     model_dir = "models"
     os.makedirs(model_dir, exist_ok=True)  # ONLY CREATE IF MODEL_DIR DOES NOT EXISTS
     filepath = os.path.join(model_dir, filename)  # model/filename
     joblib.dump(model, filepath)
+    logging.info(f"saved the trained model {filepath}")
+
 
 
 def save_plot(df, file_name, model):
+    """
+    :param df: its a dataframe
+    :param file_name: its path to save the plot
+    :param model: trained model
+    """
     def _create_base_plot(df):
+        logging.info("creating the base plot")
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
         plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
         plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -44,6 +51,7 @@ def save_plot(df, file_name, model):
         figure.set_size_inches(10, 8)
 
     def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        logging.info("plotting the decision regions")
         colors = ("red", "blue", "lightgreen", "gray", "cyan")
         colormap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -55,8 +63,6 @@ def save_plot(df, file_name, model):
 
         xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                                np.arange(x2_min, x2_max, resolution))
-        print(xx1)
-        print(xx1.ravel())
         Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
         Z = Z.reshape(xx1.shape)
         plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=colormap)
@@ -70,3 +76,4 @@ def save_plot(df, file_name, model):
     os.makedirs(plot_dir, exist_ok=True)  # ONLY CREATE IF MODEL_DIR DOES NOT EXISTS
     plotPath = os.path.join(plot_dir, file_name)  # model/filename
     plt.savefig(plotPath)
+    logging.info(f"saving the plot at {plotPath}")
